f80e389f290f799f51a0013fcfab1cef59ad5492
Abdul Wasay <wasay.adnan15431@gmail.com>
2026-06-04 16:02:10 +0500

    files changed

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-// Q1 //
+Q1 //
 
 alert("Wasay")
 
-// Q2 //
+Q2 //
 
 let num = prompt("Enter Your Name")
 alert("Welcome Your Honor " + num)
 
-// Q3 //
+Q3 //
 
 const age = 14
 alert(age)
 
-// Q4 //
+Q4 //
 
 let num1 = prompt("Enter A number")
 if (num1 >= 0) { alert("Your number in Positive") }
 else { alert("Your Number is Negative") }
 // 
-Q5 //
+// Q5 //
 
 let num1 = prompt("enter your marks")
 if(num1 >= 50){alert("You are pass good job")}
 else{alert ("You are a failure")}
 
-// Q6 //
+Q6 //
 
 let fruits = ["apple","banana","mango","orange","grapes"]
 console.log(fruits)
 
-// Q7 //
+Q7 //
 
 let colors = []
 colors.push("red")
@@ -37,26 +37,26 @@ colors.push("green")
 colors.push("blue")
 console.log(colors)
 
-// Q8 //
+Q8 //
 
 let language = ["HTML","CSS","JS"]
 language.pop()
 console.log(language)
 
-// Q9 //
+Q9 //
 
 let name = ["Ali","Ahmed","Sara","Ayesha"]
 console.log(name.length)
 
-// Q10 //
+Q10 //
 
 for (let i = 0; i <= 10; i++) { console.log(i) } 
 
-// Q11 //
+Q11 //
 
 for (let i = 10; i >= 0; i--) { console.log(i) }
 
-// Q12 //
+Q12 //
 
 let fruits = ["apple", "banana", "mango", "orange"];
 
@@ -66,7 +66,7 @@ for (let i = 0; i < fruits.length; i++) {
 
 console.log("<br><br>");
 
-// Q13 //
+Q13 //
 
 for (let i = 1; i <= 20; i++) {
 
@@ -75,7 +75,7 @@ for (let i = 1; i <= 20; i++) {
     }
 }
 
-// Q14 //
+Q14 //
 
 let cities = ["Karachi", "Lahore", "Islamabad", "Peshawar"];
 let cityName = prompt("Enter a city name:");
@@ -90,7 +90,7 @@ console.log(cityName + " is not available in the array.");
     }
 }
 
-Q15
+// Q15
 
 let numbers = [];
 
@@ -108,7 +108,7 @@ for (let j = 1; j < numbers.length; j++) {
 
 alert("Largest number is: " + largest);
 
-Q16
+// Q16
 
 let numbers = [];
 
